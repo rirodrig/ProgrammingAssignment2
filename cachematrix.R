9e0dc1bfda089681e28b88a04d3d419eee93223a
rirodrig <rirodrig@hotmail.com>
2015-02-22 16:56:56 -0500

    Final version
    RR

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Coursera R Course Feb 22, 2015
+## Assignment 2 - Lexical Scoping
+## Student: R. Rodriguez
+##
+## This function defines the base cached matrix
 makeCacheMatrix <- function(x = matrix()) {
+  elc <- NULL
+  set <- function(y) {
+    x <<- y
+    elc <<- NULL
+  }
+  get <- function() x
+  setsolve<- function(solve) elc <<- solve
+  getsolve <- function() elc
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  }
 
-}
-
-
-## Write a short comment describing this function
-
+## This function solves for the inverse of the matrix 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  elc <- x$getsolve()
+  if(!is.null(elc)) {
+    message("getting cached inverse matrix")
+    return(elc)
+  }
+  matrix <- x$get()
+  elc <- solve(matrix, ...)
+  x$setsolve(elc)
+  elc
 }
